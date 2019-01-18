f7b0dadca700da5e5194e593c4f7fd301f1f11dc
risulkarimx1 <risulkarimx1@gmail.com>
2019-01-18 23:50:34 +0100

    [First push] Lets use Zeject

---
+++ ZenjectUdemy/Assets/Scripts/GameController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+public class GameController : MonoBehaviour
+{
+    [Inject] Player player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(player.GetLocation());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
