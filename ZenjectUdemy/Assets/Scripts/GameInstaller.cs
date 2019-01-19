fba5546fdb7c3f7f37d71f943d19bbc3f205e63e
risulkarimx1 <risulkarimx1@gmail.com>
2019-01-19 01:20:23 +0100

    [13] Game Installer added

---
+++ ZenjectUdemy/Assets/Scripts/GameInstaller.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+public class GameInstaller : MonoInstaller
+{
+    [SerializeField] private GameObject _playerPrefab;
+
+    public override void InstallBindings()
+    {
+        Container.Bind<Player>().
+            FromComponentInNewPrefab(_playerPrefab).AsTransient();
+    }
+   
+}
